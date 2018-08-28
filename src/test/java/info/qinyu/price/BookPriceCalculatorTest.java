eeeb07f816f644a17dc63a9fe8b2714e2ccc8462
qinyu <qinyusuain@gmail.com>
2018-08-28 10:25:25 +0800

    Step1. Add first test method. Show maven dependency and IDE shortcuts.

--- src/test/java/info/qinyu/price/BookPriceCalculatorTest.java
+++ src/test/java/info/qinyu/price/BookPriceCalculatorTest.java
@@ -1,5 +1,27 @@
+package info.qinyu.price;
+
+import info.qinyu.book.Book;
+import info.qinyu.currency.CurrencyService;
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class BookPriceCalculatorTest {
+
+    @Test
+    public void should_calculate_book_price_in_usd() {
+        // Given
+        BookPriceCalculator calculator = new BookPriceCalculator(new CurrencyService() {
+            @Override
+            public double getExchangeForCurrency(String currency) throws IllegalArgumentException {
+                return 0.15;
+            }
+        });
+        // When
+        BookPrice bookPrice = calculator.calculatePriceInCurrency(new Book("ABook", 10000), "usd");
+        // Then
+        assertEquals(bookPrice.getPrice(), "15.00");
+        assertEquals(bookPrice.getCurrency(), "usd");
+    }
 
 }
